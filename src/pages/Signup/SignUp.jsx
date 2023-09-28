29e6543ec11b4ee6010d63a65fd4e51b76ce0144
jizrix <jizrix@gmail.com>
2023-09-29 01:59:33 +0700

    add font to project and create login page also test pass value

--- src/pages/Signup/SignUp.jsx
+++ src/pages/Signup/SignUp.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const SingUp = () => {
+  let { state } = useLocation();
+
   return (
-    <div>This is SingUp Page</div>
+    <div>
+      <div>This is SingUp Page</div>
+      <div>{ state.user + state.pass }</div>
+    </div>
   )
 }
 
