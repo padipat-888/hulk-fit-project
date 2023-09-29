15e3fb5800b448cf20ab57134cd4da71de532b43
jizrix <jizrix@gmail.com>
2023-09-29 08:26:46 +0700

    Add components folder + timer stopwatch  function

--- src/pages/ActivityTiming/ActivityTiming.jsx
+++ src/pages/ActivityTiming/ActivityTiming.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Stopwatch from '../../components/functions/Stopwatch'
+
 
 const ActivityTiming = () => {
   return (
+    <div>
     <div>This is ActivityTiming Page</div>
+    <Stopwatch/>
+    </div>
   )
 }
 
