3bee6bb58e77eb5ec65b5735f9e76c1a09a8aad9
GeeGina <giginaginaa@gmail.com>
2023-09-30 04:03:08 +0700

    "Link page ActivityType & ActivityForm , Add bg"

--- src/pages/ActivityForm/ActivityForm.jsx
+++ src/pages/ActivityForm/ActivityForm.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useLocation } from 'react-router-dom';
+import user from "../../../src/assets/user.png"
 
 const ActivityForm = () => {
+  let { state } = useLocation();
+  const [activityName, setActivityName] = useState("");
+  const [activityDes, setActivityDes] = useState("")
+  const [uploadFile, setUploadFile] = useState( user );
+
+  const handleFileChange = (evl) => { 
+    const file = evl.target.files[0]
+    const url = URL.createObjectURL(file)
+    setUploadFile(url)
+  }
+
+  const addActivity = async () => {
+    const newActivity = {
+      idActivity: 1,
+      type: state.type,
+      image: uploadFile,
+      activity: activityName,
+      desc: activityDes,
+      start: new Date()
+    };
+    console.log(newActivity)
+
+    alert("Success")
+  };
+
   return (
-    <div>This is ActivityForm Page</div>
+    <div className="bg-[url('/src/assets/bg.png')] text-white">
+    <div className="w-[328px] flex flex-col m-auto">
+      <img className="w-[93px] h-[37] mt-16	self-end" src={ state.icon } />
+      
+      <label htmlFor="fileUpload" className="button">
+      <img src={uploadFile} alt="user image" className="w-[180px] h-[180px] mt-[30px] mb-[34px] m-auto	"/>
+      </label>
+      <input
+        type="file"
+        id="fileUpload"
+        style={{ display: 'none' }}
+        onChange={handleFileChange}
+      />
+ 
+      <label htmlFor="activity-name" className="my-0.5 px-3 text-2xl ">Activity Name</label>
+      <input
+        className="my-0.5 border-2 border-[#243c5a] rounded-lg bg-inherit p-3.5  hover:border-white" id="activity-name"
+        name="activity-name"
+        placeholder="Enter Activity name"
+        value={activityName}
+        onChange={(evl) => setActivityName(evl.target.value)}
+      ></input>
+
+      <label htmlFor="activity-description" className="my-0.5 px-3 text-2xl ">Activity Description</label>
+      <textarea
+        className="h-[185px] my-0.5 border-2 border-[#243c5a] rounded-lg bg-inherit p-3.5  hover:border-white"
+        id="activity-description"
+        name="activity-description"
+        rows="4"
+        cols="50"
+        placeholder="Enter Activity details"
+        value={activityDes}
+        onChange={(evl) => setActivityDes(evl.target.value)}
+      ></textarea>
+      <button onClick={addActivity} className="h-[57px] my-16 bg-[#00ECFF] rounded-lg text-black text-2xl	font-bold	">Start</button>
+</div>
+</div>
   )
 }
 
--- src/pages/ActivityType/ActivityType.jsx
+++ src/pages/ActivityType/ActivityType.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import iconWalk from "../../../src/assets/icon/icon-walk.png"
+import iconRun from "../../../src/assets/icon/icon-run.png"
+import iconBicycle from "../../../src/assets/icon/icon-bicycle.png"
+import iconSwim from "../../../src/assets/icon/icon-swim.png"
+import iconTennis from "../../../src/assets/icon/icon-tennis.png"
+import iconSkateboard from "../../../src/assets/icon/icon-skateboard.png"
+import iconHike from "../../../src/assets/icon/icon-hike.png"
+import iconFootball from "../../../src/assets/icon/icon-football.png"
+
+import walk from "../../../src/assets/type/walk.png"
+import run from "../../../src/assets/type/run.png"
+import bicycle from "../../../src/assets/type/bicycle.png"
+import swim from "../../../src/assets/type/swim.png"
+import tennis from "../../../src/assets/type/tennis.png"
+import skateboard from "../../../src/assets/type/skateboard.png"
+import hike from "../../../src/assets/type/hike.png"
+import football from "../../../src/assets/type/football.png"
+
 
 const ActivityType = () => {
   return (
-    <div>This is ActivityType Page</div>
+    <div className="bg-[url('/src/assets/bg.png')]">
+    <div className="w-[290px] m-auto">
+      <h1 className="text-5xl text-white text-center mt-24 mb-11">Activity Type</h1>
+      <div className="grid grid-cols-2 justify-items-center">
+        <Link to="/activityform" state={ { icon: walk , type: "walk"}}><img src={iconWalk} alt="walk icon" className="w-[123px] h-[123px] m-2"/></Link>
+        <Link to="/activityform" state={ { icon: run , type: "run"}} ><img src={iconRun} alt="run icon" className="w-[123px] h-[123px] m-2" /></Link>
+        <Link to="/activityform" state={ { icon: bicycle , type: "bicycle"}} ><img src={iconBicycle} alt="bicycle icon" className="w-[123px] h-[123px] m-2" /></Link>
+        <Link to="/activityform" state={ { icon: swim , type: "swim"}} ><img src={iconSwim} alt="swim icon" className="w-[123px] h-[123px] m-2"/></Link>
+        <Link to="/activityform" state={ { icon: tennis , type: "tennis"}} ><img src={iconTennis} alt="tennis icon" className="w-[123px] h-[123px] m-2"/></Link>
+        <Link to="/activityform" state={ { icon: skateboard , type: "skateboard"}} ><img src={iconSkateboard} alt="skateboard icon" className="w-[123px] h-[123px] m-2"/></Link>
+        <Link to="/activityform" state={ { icon: hike , type: "hike"}} ><img src={iconHike} alt="hike icon" className="w-[123px] h-[123px] m-2" /></Link>
+        <Link to="/activityform" state={ { icon: football , type: "football"}} ><img src={iconFootball} alt="football icon" className="w-[123px] h-[123px] m-2"/></Link>
+        
+      </div>
+    </div>
+    </div>
   )
 }
 
