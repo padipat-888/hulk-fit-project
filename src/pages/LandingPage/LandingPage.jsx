84592eeca24b48d4989550119eb11884fd9fed53
Ariyawat-N <133693268+Ariyawat-N@users.noreply.github.com>
2023-09-29 23:48:46 +0700

    finished landingpage ui

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
+
+
 const LandingPage = () => {
   return (
-    <div>This is LandingPage Page</div>
+    
+    <div className="bg-[url('/public/assets/landingpageBg.png')] h-screen bg-no-repeat">
+      <div className='pl-12 pt-16'>
+        <h1 className=' text-[64px] text-cyan-400 font-semibold'>HULK FIT</h1>
+        <h3 className=' text-white font-normal'>The App That Builds <span className='text-cyan-400 font-bold'>More Than</span> Muscles</h3>
+      </div>
+      <div className='w-screen h-1/4 absolute bottom-0 flex justify-evenly items-center'>
+        <button className='text-black font-semibold bg-[#00ECFF] h-12 w-28 rounded-full text-xl'>LOGIN</button>
+        <button className='text-black font-semibold bg-[#F540A1] h-12 w-28 rounded-full text-xl'>SIGN UP</button>
+      </div>
+
+    </div>
   )
 }
 
